5b8e768a66258c03360ee09208b4757e16057a35
heyuheyun <542745903@qq.com>
2022-10-01 18:00:20 +0800

    改善侧边栏缩略显示

--- src/views/layout/components/sidebar/ItemMenu.vue
+++ src/views/layout/components/sidebar/ItemMenu.vue
@@ -3,15 +3,20 @@
     @mouseenter="changeTip(item,$event)" @mouseleave="changeTip(false,$event)">
         <span class="tab" v-for="i in tab"></span>
         <i class="iconfont icon" :class="item.meta.icon"></i>
+        <span class="no-icon" v-if="!item.meta.icon" v-show="menuHover">
+            <p>{{item.meta.title.slice(0,4)}}</p>
+        </span>
         <span v-show="!menuHover">{{ item.meta.title }}</span>
     </a>
 
     <template v-else>
         <div class="menu-title" @click="menuclick" @mouseenter="changeTip(item,$event)" @mouseleave="changeTip(false,$event)">
             <span class="tab" v-for="i in tab"></span>
-            <i class="iconfont icon" :class="item.meta.icon"></i>
+            <i class="iconfont icon" :class="item.meta.icon">
+                <i :class="{'right-bottom':iconFold,'left-top':!iconFold}" v-show="menuHover"></i>
+            </i>
             <span v-show="!menuHover">{{ item.meta.title }}</span>
-            <i class="iconfont icon icon-unfold" v-show="!menuHover" ref="foldIcon"></i>
+            <i class="iconfont icon" v-show="!menuHover" :class="{'icon-unfold':iconFold,'icon-packup':!iconFold}"></i>
         </div>
         <div class="submenu">
             <!-- 递归调用以支持多级路由菜单 -->
@@ -28,7 +33,7 @@ import { useRouter, useRoute } from "vue-router";
 
 const router = useRouter();
 const route = useRoute();
-const foldIcon = ref();
+var iconFold=ref(true);
 
 const props = defineProps({
     item: {
@@ -57,9 +62,8 @@ function routeTo(item) {
 
 //展开或折叠子菜单
 function menuclick(event) {
+    iconFold.value=!iconFold.value;
     let menu = event.currentTarget.nextElementSibling;
-    if (foldIcon.value.className == "iconfont icon icon-unfold") foldIcon.value.className = "iconfont icon icon-packup";
-    else foldIcon.value.className = "iconfont icon icon-unfold";
 
     if (menu.style.maxHeight == "0px" || menu.style.maxHeight === "") {
         let maxheight = getELHeigth(menu.children);
@@ -113,6 +117,44 @@ function changeTip(item,event){
     .icon {
         font-size: 20px;
         margin: 0px 20px;
+        position:relative;
+
+        .right-bottom,.left-top{
+            position:absolute;
+            display:inline-block;
+            opacity: .3;
+            bottom:-5px;
+            right:-12px;
+            width:0;
+            height:0;
+            border-width: 4px 4px 0px 4px;
+            border-color: @theme-sider-font transparent transparent transparent;
+            border-style: solid;
+        }
+        .left-top{
+            border-width: 0px 4px 4px 4px;
+            border-color: transparent transparent @theme-sider-font transparent;
+        }
+    }
+
+    .no-icon{
+        position:relative;
+        width:40px;
+        opacity: .7;
+        left:-30px;
+        text-align: center;
+        flex:0 0 auto;
+
+        p{
+            display: inline-block;
+            margin-left: 5px;
+            font-size: 10px;
+            max-width: 40px;
+            line-height: 18px;
+            letter-spacing:.4em;
+            font-weight: bold;
+            white-space: pre-wrap;
+        }
     }
 
     span {
